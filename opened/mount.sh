941a95b9cd4cb2f04e6374c505dc9d05dc00df8c
froggitti <85637569+froggitti@users.noreply.github.com>
2025-09-02 00:02:52 -0400

    add check for if its actually vector

--- opened/mount.sh
+++ opened/mount.sh
@@ -6,6 +6,10 @@ fi
 
 if [ -f apq8009-robot-sysfs.img ]; then
   sudo mount apq8009-robot-sysfs.img mounted/
+  if [ ! -d mounted/anki ]; then
+    echo this is not a vector ota sysfs file.
+    exit 1
+  fi
 else
   echo sysfs not found.
 fi
