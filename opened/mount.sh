7a592ff3caf7197a8a715601526e5ae8ce60d10a
froggitti <85637569+froggitti@users.noreply.github.com>
2025-09-01 23:59:00 -0400

    b

--- opened/mount.sh
+++ opened/mount.sh
@@ -1,2 +1,11 @@
-mkdir mounted
-sudo mount apq8009-robot-sysfs.img mounted/
+#!/usr/bin/env bash
+
+if [ ! -d mounted ]; then
+  mkdir mounted
+fi
+
+if [ -f apq8009-robot-sysfs.img ]; then
+  sudo mount apq8009-robot-sysfs.img mounted/
+else
+  echo sysfs not found.
+fi
