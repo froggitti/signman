59f3a9636d117c5db72d33483e53d3a14dc6327f
froggitti <85637569+froggitti@users.noreply.github.com>
2025-09-01 23:54:38 -0400

    add check for every OTA file

--- unpackota.sh
+++ unpackota.sh
@@ -5,11 +5,23 @@ if [ "$1" == "" ]; then
 else
   echo opening ota
   tar -xf $1
-  if [ -e "opened/apq8009*" ]; then
-    rm opened/apq8009-*
+  if [ -f "opened/apq8009-robot-sysfs.img" ]; then
+    sudo rm opened/apq8009-robot-sysfs.img
   fi
-  if [ -e "opened/manifest.*" ]; then
-    rm opened/manifest.*
+  if [ -f "opened/apq8009-robot-sysfs.img.gz" ]; then
+    sudo rm opened/apq8009-robot-sysfs.img.gz
+  fi
+  if [ -f "opened/apq8009-robot-boot.img" ]; then
+    sudo rm opened/apq8009-robot-boot.img
+  fi
+  if [ -f "opened/apq8009-robot-boot.img.gz" ]; then
+    sudo rm opened/apq8009-robot-boot.img.gz
+  fi
+  if [ -f "opened/manifest.ini" ]; then
+    sudo rm opened/manifest.ini
+  fi
+  if [ -f "opened/manifest.sha256" ]; then
+    sudo rm opened/manifest.sha256
   fi
   sudo mv manifest.* opened
   sudo mv apq8009-* opened
